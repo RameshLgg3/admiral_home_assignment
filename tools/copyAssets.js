b9d91ce2b80003a51a74cd9d00943d9b94501eaf
RameshLgg3 <111238882+RameshLgg3@users.noreply.github.com>
2022-08-28 09:07:42 +0530

    view

--- tools/copyAssets.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var shell = require("shelljs");
-// Copy all the view templates
-shell.cp("-R", "src/views", "dist/");
-//# sourceMappingURL=copyAssets.js.map
